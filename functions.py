e8a975cedee71e2862f8257d514970b8691cab30
Keter22 <mathewketer22@gmail.com>
2024-07-24 17:40:25 +0300

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,5 +1,6 @@
 # importing relevant libraries
 import pandas as pd
+import numpy as np
 
 
 class Data_Loader:
@@ -31,10 +32,70 @@ class Data_Informer(Data_Loader):
         for column, dtype in data_types.items():
             print(f"{column}: {dtype}")
     
-class Data_cleaning:
-    def __init__(self):
-        pass
     
+
+class DataCleaner:
+    def __init__(self, data):
+        self.data = data
+    
+    def remove_null_values(self):
+        self.data = self.data.dropna()
+        return self.data
+    
+    def remove_outliers(self, column_name):
+        Q1 = self.data[column_name].quantile(0.25)
+        Q3 = self.data[column_name].quantile(0.75)
+        IQR = Q3 - Q1
+        lower_bound = Q1 - 1.5 * IQR
+        upper_bound = Q3 + 1.5 * IQR
+        self.data = self.data[~((self.data[column_name] < lower_bound) | (self.data[column_name] > upper_bound))]
+        return self.data
+    
+    def fill_missing_values(self, strategy='mean'):
+        if strategy == 'mean':
+            self.data = self.data.fillna(self.data.mean())
+        elif strategy == 'median':
+            self.data = self.data.fillna(self.data.median())
+        elif strategy == 'mode':
+            self.data = self.data.fillna(self.data.mode().iloc[0])
+        return self.data
+
+
+
+    
+class DataPreparer:
+    def __init__(self, data):
+        self.data = data
+    
+    def melt_data(self):
+        melted = pd.melt(self.data, id_vars=['RegionID', 'RegionName', 'City', 'State', 'Metro', 'CountyName', 'SizeRank'], var_name='time', value_name='value')
+        melted['time'] = pd.to_datetime(melted['time'], infer_datetime_format=True)
+        melted = melted.dropna(subset=['value'])
+        return melted
+    
+    def check_null_values(self):
+        null_counts = self.data.isnull().sum()
+        return null_counts
+    
+    def missing_values_percentage(self):
+        total = self.data.isnull().sum().sort_values(ascending=False)
+        percent = (self.data.isnull().sum() / self.data.isnull().count() * 100).sort_values(ascending=False)
+        missing_data = pd.concat([total, percent], axis=1, keys=['Total', 'Percent'])
+        return missing_data
+    
+    def check_outliers(self, column_name):
+        Q1 = self.data[column_name].quantile(0.25)
+        Q3 = self.data[column_name].quantile(0.75)
+        IQR = Q3 - Q1
+        lower_bound = Q1 - 1.5 * IQR
+        upper_bound = Q3 + 1.5 * IQR
+        outliers = self.data[(self.data[column_name] < lower_bound) | (self.data[column_name] > upper_bound)]
+        return outliers
+    
+    def modify_column_names(self):
+        self.data.columns = [col.lower().replace(' ', '_') for col in self.data.columns]
+        return self.data
+       
 class Analysis:
     def __init__(self):
         pass
